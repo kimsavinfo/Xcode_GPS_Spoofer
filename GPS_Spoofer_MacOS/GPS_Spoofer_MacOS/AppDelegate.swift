22f21706ae2d8051e993e9235ea0470603ef7ccc
Kim <ksavaroche@clayair.io>
2019-07-20 07:56:03 +0200

    add macos project

---
+++ GPS_Spoofer_MacOS/GPS_Spoofer_MacOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GPS_Spoofer_MacOS
+//
+//  Created by Kim SAVAROCHE on 20/07/2019.
+//  Copyright © 2019 Kim SAVAROCHE. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GPS_Spoofer_MacOS/GPS_Spoofer_MacOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GPS_Spoofer_MacOS
+//
+//  Created by Kim SAVAROCHE on 20/07/2019.
+//  Copyright © 2019 Kim SAVAROCHE. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
